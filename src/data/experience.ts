4c7c0492443968ead3559d0071d1be545ab4628d
Nwtnn <newtontiwari4.nt@gmail.com>
2024-08-15 23:15:56 +0545

    rashihangbum

---
+++ src/data/experience.ts
@@ -0,0 +1,49 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "November 2023 — Present",
+    title: "Project",
+    company: "Game Developement",
+    companyUrl: "",
+    description:
+      "To create games that captivate players and offer unique experiences. Whether I’m prototyping a new idea or polishing a final product, I’m always excited about the endless possibilities that Unity offers for game development.",
+    technologies: [
+      "Unity game engine ",
+      "c#",
+      "Blender",
+      "Photoshop",
+    ],
+    logo: "",
+  },
+  {
+    dates: "Feb 2023 — March 2023",
+    title: "Intern",
+    company: "Hamro patro",
+    companyUrl: "https://www.hamropatro.com/",
+    description:
+      "Networking",
+    technologies: [
+      "Data Processing Tools",
+      "Microsoft Office Suite",
+      "Database Management Systems",
+    ],
+    logo: "",
+  },
+  {
+    dates: "April 2023 — Sept 2023",
+    title: "Intern",
+    company: "Deerhold.ltd",
+    companyUrl: "https://deerhold.com/",
+    description:
+      "",
+    technologies: [
+      "Web Scraping",
+      "Python",
+      "BeautifulSoup",
+      "SQL Databases",
+
+    ],
+    logo: "",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,27 @@
+import IconDribbble from "../components/icons/dribbble.astro";
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/Nwtnn",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/newton-tiwari-7078a3263/",
+    icon: IconLinkedin,
+  },
+  // {
+  //   name: "Twitter",
+  //   url: "",
+  //   icon: IconTwitter,
+  // },
+  // // {
+  //   name: "Dribbble",
+  //   url: "",
+  //   icon: IconDribbble,
+  // },
+];
